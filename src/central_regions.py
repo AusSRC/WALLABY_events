23927235b9c98835f35785d3286240ee5c9bddfa
Austin Shen <austin.shen@csiro.au>
2022-06-02 08:16:12 +0000

    logging, rename processing name in database and remove database entry on job submission

--- src/central_regions.py
+++ src/central_regions.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import logging
 import sys
 import json
 import asyncio
@@ -9,6 +10,9 @@ import numpy as np
 
 from utils import parse_config
 from workflow_publisher import WorkflowPublisher
+
+
+logging.basicConfig(level=logging.INFO)
 
 
 def generate_tile_uuid(ra, dec):
@@ -29,7 +33,7 @@ def get_footprint_pairs(observations, distance_threshold=0.7):
     distance_matrix = np.zeros((N, N))
     for i in range(N):
         for j in range(N):
-            # Distance needs to be calculated differently (angular separation)
+            # TODO(austin): use angular separation rather than euclidean distance
             distance_matrix[i, j] = np.linalg.norm(coord_array[i] - coord_array[j])
 
     # identify pairs based on threshold
@@ -77,6 +81,8 @@ async def process_centre_regions(loop):
             WHERE phase = 'Pilot 2' \
             AND quality = 'PASSED'"
         )
+        for obs in res:
+            logging.info(f"Observation: {obs}")
         pairs = get_footprint_pairs(res)
         matches = add_footprints_to_tiles(res, pairs)
 
@@ -84,6 +90,7 @@ async def process_centre_regions(loop):
             pair, match = v
             A_idx, B_idx = pair
             A_id, B_id, uuid = match
+            logging.info(f"Found observation pairs for tile {uuid}: ({A_id}, {B_id}) with list index values ({A_idx}, {B_idx}) in PostgreSQL query response")
             # Update database
             await conn.fetch(
                 'UPDATE wallaby.tile \
@@ -95,7 +102,7 @@ async def process_centre_regions(loop):
             # See if there is a completed post-processing job for the same tile
             completed_job = await conn.fetch(
                 "SELECT * FROM wallaby.postprocessing \
-                WHERE run_name = $1 AND status = 'COMPLETED'",
+                WHERE name = $1 AND status = 'COMPLETED'",
                 uuid
             )
 
@@ -105,28 +112,24 @@ async def process_centre_regions(loop):
 
             # Submit a post-processing job
             if not completed_job:
-                # TODO: logging submitting job
+                ra_centre = (float(res[A_idx]['ra']) + float(res[B_idx]['ra'])) / 2
+                dec_centre = (float(res[A_idx]['dec']) + float(res[B_idx]['dec'])) / 2
+                region = f"{ra_centre - 2.0}, {ra_centre + 2.0}, {dec_centre - 2.0}, {dec_centre + 2.0}"
+                logging.info(f"Submitting job for tile {uuid} to process region: {region}")
                 params = {
                     'username': 'ashen',
                     'pipeline_key': workflow_keys['postprocessing_key'],
                     'params': {
                         "RUN_NAME": uuid,
+                        "REGION": region,
                         "FOOTPRINTS": f"{res[A_idx]['image_cube_file']}, {res[B_idx]['image_cube_file']}",
                         "WEIGHTS": f"{res[A_idx]['weights_cube_file']}, {res[B_idx]['weights_cube_file']}"
                     }
                 }
                 msg = json.dumps(params).encode()
                 await publisher.publish(msg)
-
-                # Add post-processing job entry to database
-                await conn.execute(
-                    "INSERT INTO wallaby.postprocessing \
-                    (run_name, status) \
-                    VALUES ($1, $2) \
-                    ON CONFLICT ON CONSTRAINT postprocessing_run_name_key \
-                    DO NOTHING;",
-                    uuid, "QUEUED"
-                )
+            else:
+                logging.info(f"Tile {uuid} already processed, skipping.")
         return
     except Exception as e:
         raise
@@ -141,8 +144,8 @@ async def _repeat(loop):
     INTERVAL = 500
 
     while True:
-        await asyncio.sleep(INTERVAL)
         await process_centre_regions(loop)
+        await asyncio.sleep(INTERVAL)
 
 
 if __name__ == '__main__':
