5b8f4598119328a8df152d5c02c6da9465c6901c
Austin Shen <austin.shen@csiro.au>
2022-06-29 08:31:05 +0000

    WAL-129: logic and tests for getting groups of 3 tiles to process

--- src/logic.py
+++ src/logic.py
@@ -8,31 +8,63 @@ examples.
   Contains the following module constants that are used throughout:
 
   - LOGIC_DECIMAL:                      Number of decimal places to round the RA and Dec values to
-  - LOGIC_ADJACENT_MAX_THRESHOLD:       Maximum threshold in RA/Dec for two tiles to be considered adjacent
-  - LOGIC_ADJACENT_MIN_THRESHOLD:       Minimum threshold in RA/Dec for two tiles to be considered adjacent
-  - LOGIC_ADJACENT_OFFSET_THRESHOLD:    Centre to slightly offset centre
+  - LOGIC_OBSERVATION_THRESHOLD:        Maximum offset for two observations for a given tile (radians)
+  - LOGIC_ADJACENT_MAX_THRESHOLD:       Maximum threshold in RA/Dec for two tiles to be considered adjacent (deg)
+  - LOGIC_ADJACENT_MIN_THRESHOLD:       Minimum threshold in RA/Dec for two tiles to be considered adjacent (deg)
+  - LOGIC_ADJACENT_OFFSET_THRESHOLD:    Centre to slightly offset centre (deg)
 """
 # TODO(austin): A diagram somewhere to explain constants...
 
 import os
 import math
 import logging
+import numpy as np
+from astropy.coordinates import angular_separation
 
 
 logging.basicConfig(level=logging.INFO)
 
 
 LOGIC_DECIMAL = os.getenv('LOGIC_DECIMAL', 1)
+LOGIC_OBSERVATION_THRESHOLD = os.getenv('LOGIC_OBSERVATION_THRESHOLD', math.radians(1.0))
 LOGIC_ADJACENT_MAX_THRESHOLD = os.getenv('LOGIC_ADJACENT_MAX_THRESHOLD', 7.0)
 LOGIC_ADJACENT_MIN_THRESHOLD = os.getenv('LOGIC_ADJACENT_MIN_THRESHOLD', 1.0)
 LOGIC_ADJACENT_OFFSET_THRESHOLD = os.getenv('LOGIC_ADJACENT_OFFSET_THRESHOLD', 4.0)
+
+
+def get_observation_pairs(observations):
+    """Get pairs of observations from a list of observations based on their
+    central RA and Dec.
+
+    Args:
+        observations: List of observation objects from database query
+
+    Returns:
+        List of tuples of pairs of observations
+    """
+    N = len(observations)
+    coord_array = [(math.radians(float(r['ra'])), math.radians(float(r['dec']))) for r in observations]
+    separation_matrix = np.zeros((N, N))
+    for i in range(N):
+        for j in range(i):
+            separation_matrix[i, j] = angular_separation(*coord_array[i], *coord_array[j])
+
+    # get pairs based on angular separation
+    pair_matrix = (separation_matrix < LOGIC_OBSERVATION_THRESHOLD)
+    pairs = []
+    for i in range(N):
+        for j in range(i):
+            if pair_matrix[i][j]:
+                pairs.append((j, i))
+
+    return pairs
 
 
 def get_adjacent_tiles(tiles):
     """Find adjacent pairs in RA or Dec bands from list of tiles.
 
     Args:
-        tiles: List of tiles from database query.
+        tiles: List of tile objects from database query.
 
     Returns:
         List of tuples for pairs of tiles.
@@ -50,6 +82,7 @@ def get_adjacent_tiles(tiles):
             # Adjacent in RA bands
             if (abs(ra_A - ra_B) < LOGIC_ADJACENT_MIN_THRESHOLD) and \
                (abs(dec_A - dec_B) < LOGIC_ADJACENT_MAX_THRESHOLD):
+                # TODO(austin): pull logging out of this...    
                 logging.info(
                     f'Found adjacent tiles {tiles[i]["identifier"]} and \
                     {tiles[j]["identifier"]} in right ascension band'
@@ -65,6 +98,30 @@ def get_adjacent_tiles(tiles):
                 )
                 pairs.append((j, i))
     return pairs
+
+
+def get_tile_groups(tiles):
+    """Find groups of three tiles for processing central regions.
+
+    Args:
+        tiles: List of tile objects from database query.
+
+    Returns:
+        List of tuples containing indices for tile groups.
+    """
+    groups = []
+    pairs = get_adjacent_tiles(tiles)
+    for pair in pairs:
+        i, j = pair
+        pair_object = {
+            'ra': (float(tiles[i]['ra']) + float(tiles[j]['ra'])) / 2.0,
+            'dec': (float(tiles[i]['dec']) + float(tiles[j]['dec'])) / 2.0,
+            'identifier': 'pair'
+        }
+        pair_rec = get_adjacent_tiles([pair_object] + tiles)
+        group = [(*pair, j - 1) for (i, j) in pair_rec if j - 1 not in pair and i == 0]  # so sorry
+        groups += group
+    return groups
 
 
 # TODO(austin): constants to named variables.
--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,4 +1,4 @@
-from src.logic import get_adjacent_tiles
+from src.logic import get_observation_pairs, get_adjacent_tiles, get_tile_groups
 
 
 PHASE_2_SAMPLE_TILES = [
@@ -18,17 +18,72 @@ FULL_SURVEY_SAMPLE_TILES = [
     {'ra': 15.254, 'dec': 24.0, 'identifier': '015+24'},
 ]
 
+# TODO(austin): check full survey strategy...
+# [
+#     {'ra': 344.745, 'dec': 24.0, 'identifier': '003+24'},
+#     {'ra': 350.847, 'dec': 24.0, 'identifier': '009+24'},
+#     {'ra': 356.949, 'dec': 24.0, 'identifier': '015+24'}
+# ]
+
+PHASE_1_SAMPLE_OBSERVATIONS = [
+    {'sbid': 10609, 'ra': 153.952, 'dec': -27.378, 'description': "Hydra 1A"},
+    {'sbid': 10269, 'ra': 154.464, 'dec': -27.823, 'description': "Hydra 1B"},
+    {'sbid': 10612, 'ra': 159.853, 'dec': -27.378, 'description': "Hydra 2A"},
+    {'sbid': 10626, 'ra': 160.366, 'dec': -27.823, 'description': "Hydra 2B"},
+    {'sbid': 10809, 'ra': 189.508, 'dec': -0.4491, 'description': "Norma 1A"},
+    {'sbid': 10812, 'ra': 189.959, 'dec': -0.8991, 'description': "Norma 1B"},
+    {'sbid': 10736, 'ra': 189.510, 'dec': 4.95179, 'description': "Norma 2A"},
+    {'sbid': 12193, 'ra': 253.875, 'dec': -59.480, 'description': "NGC4636 2A"},
+    {'sbid': 12209, 'ra': 254.789, 'dec': -59.914, 'description': "NGC4636 2B"},
+    {'sbid': 11816, 'ra': 244.145, 'dec': -59.480, 'description': "NGC4636 1A"},
+    {'sbid': 11832, 'ra': 245.059, 'dec': -59.914, 'description': "NGC4636 1B"},
+]
+
+
+PHASE_2_SAMPLE_OBSERVATIONS = [
+    {'sbid': 33714, 'ra': 195.359, 'dec': 5.547, 'description': "NGC4808 B old observation"},
+    {'sbid': 33681, 'ra': 194.906, 'dec': 5.998, 'description': "NGC4808 A"},
+    {'sbid': 37604, 'ra': 195.359, 'dec': 5.547, 'description': "NGC4808 B"},
+    {'sbid': 33879, 'ra': 197.501, 'dec': -13.155, 'description': "NGC5044 Tile 1A"},
+    {'sbid': 25701, 'ra': 203.256, 'dec': -22.144, 'description': "NGC5044 Tile 4A"},
+    {'sbid': 25750, 'ra': 203.748, 'dec': -22.593, 'description': "NGC5044 Tile 4B"},
+    {'sbid': 34166, 'ra': 197.500, 'dec': -18.550, 'description': "NGC5044 Tile 2A"},
+    {'sbid': 34302, 'ra': 197.964, 'dec': -13.602, 'description': "NGC5044 Tile 1B"},
+    {'sbid': 34275, 'ra': 197.979, 'dec': -18.999, 'description': "NGC5044 Tile 2B"},
+    {'sbid': 31536, 'ra': 203.129, 'dec': -16.749, 'description': "NGC5044 Tile 3A"},
+    {'sbid': 40905, 'ra': 204.064, 'dec': -16.729, 'description': "NGC5044 Tile 3B"}
+]
+
+
+def test_get_correct_observation_pairs():
+    """Test get_observation_pairs function to make sure it works for
+    Pilot Phase1 and Phase 2.
+
+    """
+    PHASE_1_PAIRS = set([(0, 1), (2, 3), (4, 5), (7, 8), (9, 10)])
+    PHASE_2_PAIRS = set([(0, 1), (0, 2), (1, 2), (3, 7), (4, 5), (6, 8), (9, 10)])
+    assert(PHASE_1_PAIRS == set(get_observation_pairs(PHASE_1_SAMPLE_OBSERVATIONS)))
+    assert(PHASE_2_PAIRS == set(get_observation_pairs(PHASE_2_SAMPLE_OBSERVATIONS)))
+
 
 def test_get_correct_adjacent_tiles():
     """Test get_adjacent_tiles function to ensure it works correctly for
     both Pilot Phase 2 and Full Survey sample tiles.
 
     """
-    PHASE_2_ADJACENT = [(2, 3), (1, 4)]
-    FULL_SURVEY_ADJACENT = [(0, 1), (1, 2), (3, 4), (4, 5)]
-    assert(PHASE_2_ADJACENT == get_adjacent_tiles(PHASE_2_SAMPLE_TILES))
-    assert(FULL_SURVEY_ADJACENT == get_adjacent_tiles(FULL_SURVEY_SAMPLE_TILES))
+    PHASE_2_ADJACENT = set([(2, 3), (1, 4)])
+    FULL_SURVEY_ADJACENT = set([(0, 1), (1, 2), (3, 4), (4, 5)])
+    assert(PHASE_2_ADJACENT == set(get_adjacent_tiles(PHASE_2_SAMPLE_TILES)))
+    assert(FULL_SURVEY_ADJACENT == set(get_adjacent_tiles(FULL_SURVEY_SAMPLE_TILES)))
+
+
+def test_get_correct_tile_group():
+    """Test get_tile_groups for getting groups of three tiles for Pilot Phase 2.
+
+    """
+    PHASE_2_TILE_GROUPS = set([(2, 3, 4), (1, 4, 3)])
+    assert(PHASE_2_TILE_GROUPS == set(get_tile_groups(PHASE_2_SAMPLE_TILES)))
 
 
 if __name__ == "__main__":
-    test_get_correct_adjacent_tiles()
+    test_get_correct_tile_group()
